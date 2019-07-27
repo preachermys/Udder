4dbfe0853295db88348beb2c3f32a5432a547e26
preachermys <milowanowa@mail.ru>
2019-07-27 03:16:38 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Preacher on 27/07/2019.
+//  Copyright © 2019 Yulia Milovanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
